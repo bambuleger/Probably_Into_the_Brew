83e512c7eb0868cba02c3e14ae907216a9bf9f99
chumiiger <jonas.dreier@googlemail.com>
2013-12-07 09:24:53 +0100

    v1.3

---
+++ functions.lua
@@ -0,0 +1,131 @@
+--[[
+    =======================================================
+                         Into the Brew
+                             v1.3
+    =======================================================
+                Brewmaster Monk Rotation for PE            
+    https://probablyengine.com/forum/viewtopic.php?id=189
+    =======================================================
+]]--
+
+-- Initialize tables
+if not intoBrew then intoBrew = {} end
+
+local playerName = UnitName("player")
+
+StaticPopupDialogs["welcome"] = {
+        maxLetters = 1200,
+        editBoxWidth = 2150,
+        OnShow = function(self)
+    end,
+    text = " Welcome back " .. playerName .. "\nInto the Brew v1.3 loaded\nPlease comment / report bugs at\nhttp://tinyurl.com/intobrew",
+    button1 = "Have fun!",
+    OnAccept = function()
+    end,
+    timeout = 20,
+    whileDead = true,
+    hideOnEscape = true,
+    preferredIndex = 3,  
+}
+StaticPopup_Show ("welcome")
+
+-- Thank you Mavmins for the code
+ProbablyEngine.listener.register("intoBrew", "MODIFIER_STATE_CHANGED", function(...)
+        local key                        = select(1, ...)
+        local isDown                = select(2, ...)
+        USE_AOE = ProbablyEngine.config.data.button_states.multitarget
+        USE_INTRPT = ProbablyEngine.config.data.button_states.interrupt
+               
+       --[[if  (key == "RSHIFT" and isDown == 1) then --cooldowns toggle
+                if USE_CDS then 
+                        USE_CDS = false 
+                        print("Cooldowns: |cFFFF0000Disabled")        
+                        ProbablyEngine.buttons.toggle('cooldowns')
+                else 
+                        USE_CDS = true 
+                        print("Cooldowns: |cFF00FF00Enabled")
+                        ProbablyEngine.buttons.toggle('cooldowns')
+                end
+        end  ]]--  
+
+        if  (key == "LCTRL" and isDown == 1) then  --aoe toggle
+                if USE_AOE then
+                        USE_AOE = false
+                        print("AoE: |cFFFF0000Disabled")
+                        ProbablyEngine.buttons.toggle('multitarget')                        
+                else
+                        USE_AOE = true
+                        print("AoE: |cFF00FF00Enabled")        
+                        ProbablyEngine.buttons.toggle('multitarget')
+                end
+        end 
+
+        if  (key == "RSHIFT" and isDown == 1) then  --interrupt toggle
+                if USE_INTRPT then
+                        USE_INTRPT = false
+                        print("Interrupts: |cFFFF0000Disabled")
+                        ProbablyEngine.buttons.toggle('interrupt')                        
+                else
+                        USE_INTRPT = true
+                        print("Interrupts: |cFF00FF00Enabled")        
+                        ProbablyEngine.buttons.toggle('interrupt')
+                end
+        end       
+end)
+ 
+function intoBrew.touchdeath ()
+    local GlyphSockets = NUM_GLYPH_SLOTS
+    if IsSpellKnown(115080)
+        and UnitHealth("target") <= UnitHealth("player") 
+        and not UnitIsPlayer("target")
+        then
+            for i=1, GlyphSockets do
+                if select(4, GetGlyphSocketInfo(i)) == 123391
+                    then return true
+                end
+            end
+        if UnitPower("player",12) >= 3
+            then return true
+        end
+     end    
+end
+
+function intoBrew.StaggerValue ()    
+    local staggerLight, _, iconLight, _, _, remainingLight, _, _, _, _, _, _, _, _, valueStaggerLight, _, _ = UnitAura("player", GetSpellInfo(124275), "", "HARMFUL")
+    local staggerModerate, _, iconModerate, _, _, remainingModerate, _, _, _, _, _, _, _, _, valueStaggerModerate, _, _ = UnitAura("player", GetSpellInfo(124274), "", "HARMFUL")
+    local staggerHeavy, _, iconHeavy, _, _, remainingHeavy, _, _, _, _, _, _, _, _, valueStaggerHeavy, _, _ = UnitAura("player", GetSpellInfo(124273), "", "HARMFUL")
+    local staggerTotal= (remainingLight or remainingModerate or remainingHeavy or 0) * (valueStaggerLight or valueStaggerModerate or valueStaggerHeavy or 0)
+    local percentOfHealth=(100/UnitHealthMax("player")*staggerTotal)
+    local ticksTotal=(valueStaggerLight or valueStaggerLight or valueStaggerLight or 0)
+    return percentOfHealth;
+end
+
+function intoBrew.DrinkStagger()
+    if (UnitPower("player", 12) >= 1 or UnitBuff("player", GetSpellInfo(138237))) then 
+        if UnitDebuff("player", GetSpellInfo(124273))
+            then return true
+        end
+        if UnitDebuff("player", GetSpellInfo(124274))
+            and intoBrew.StaggerValue() > 25
+            then return true
+        end
+        --[[if UnitDebuff("player", GetSpellInfo(124275))
+            and intoBrew.StaggerValue() > 3
+            then return true
+        end]]--
+    end
+    return false
+end
+
+local PlayerHP = 100 * UnitHealth("player") / UnitHealthMax("player")
+
+function intoBrew.Healthstone(...)
+    if PlayerHP < 40
+    and GetItemCount(5512,false,false) > 0 
+    and ( select(2, GetItemCooldown(5512)) == 0 ) then
+        return true
+    end
+end
+ 
+-- Register library
+ProbablyEngine.library.register("intoBrew", intoBrew)
--- rotation.lua
+++ rotation.lua
@@ -1,58 +1,13 @@
 --[[
 	=======================================================
 	                     Into the Brew
-	                         v 1.2
+	                         v1.3
 	=======================================================
 	            Brewmaster Monk Rotation for PE            
-	https://probablyengine.com/forum/viewtopic.php?pid=550
+	https://probablyengine.com/forum/viewtopic.php?id=189
 	=======================================================
 ]]--
 
-intoBrew = { }
-
-function intoBrew.touchdeath ()
-	local GlyphSockets = NUM_GLYPH_SLOTS
-	if IsSpellKnown(115080)
-		and UnitHealth("target") <= UnitHealth("player") 
-		and not UnitIsPlayer("target")
-		then
-			for i=1, GlyphSockets do
-				if select(4, GetGlyphSocketInfo(i)) == 123391
-					then return true
-				end
-			end
-		if UnitPower("player",12) >= 3
-			then return true
-		end
- 	 end	
-end
-
-function intoBrew.StaggerValue ()    
-    local staggerLight, _, iconLight, _, _, remainingLight, _, _, _, _, _, _, _, _, valueStaggerLight, _, _ = UnitAura("player", GetSpellInfo(124275), "", "HARMFUL")
-  	local staggerModerate, _, iconModerate, _, _, remainingModerate, _, _, _, _, _, _, _, _, valueStaggerModerate, _, _ = UnitAura("player", GetSpellInfo(124274), "", "HARMFUL")
-    local staggerHeavy, _, iconHeavy, _, _, remainingHeavy, _, _, _, _, _, _, _, _, valueStaggerHeavy, _, _ = UnitAura("player", GetSpellInfo(124273), "", "HARMFUL")
-    local staggerTotal= (remainingLight or remainingModerate or remainingHeavy or 0) * (valueStaggerLight or valueStaggerModerate or valueStaggerHeavy or 0)
-    local percentOfHealth=(100/UnitHealthMax("player")*staggerTotal)
-    local ticksTotal=(valueStaggerLight or valueStaggerLight or valueStaggerLight or 0)
-    return percentOfHealth;
-end
-
-function intoBrew.DrinkStagger()
-	if (UnitPower("player", 12) >= 1 or UnitBuff("player", GetSpellInfo(138237))) then 
-		if UnitDebuff("player", GetSpellInfo(124273))
-			then return true
-		end
-		if UnitDebuff("player", GetSpellInfo(124274))
-			and intoBrew.StaggerValue() > 25
-			then return true
-		end
-		--[[if UnitDebuff("player", GetSpellInfo(124275))
-			and intoBrew.StaggerValue() > 3
-			then return true
-		end]]--
-	end
-	return false
-end
 
 ProbablyEngine.rotation.register_custom(268, "Into the Brew", {
 
@@ -60,17 +15,17 @@ ProbablyEngine.rotation.register_custom(268, "Into the Brew", {
 	{ "pause", "modifier.lshift"},
  	{ "115180", "modifier.lalt", "ground" }, -- Dizzying Haze
  	{ "115315", "modifier.rcontrol", "ground" }, -- Black Ox Statue
- 	{ "122057", "modifier.lcontrol", "target"}, -- Clash
+
 
 --Buff
 	{ "115921", -- Legacy of the Emperor
-    {
-      "!player.buff(117666).any", -- Legacy of the Emperor Buff
-      "!player.buff(1126).any", -- Mark of the Wild
-      "!player.buff(90363).any", -- Embrace of the Shale Spider
-      "!player.buff(20217).any" -- Blessing of Kings
-    }
-  },
+    	{
+      	"!player.buff(117666).any", -- Legacy of the Emperor Buff
+      	"!player.buff(1126).any", -- Mark of the Wild
+      	"!player.buff(90363).any", -- Embrace of the Shale Spider
+      	"!player.buff(20217).any" -- Blessing of Kings
+    	}
+ 	},
 
 -- Interrupts
 	{ "117368", "modifier.interrupts" }, --Grapple Weapon (Disarm)
@@ -83,9 +38,13 @@ ProbablyEngine.rotation.register_custom(268, "Into the Brew", {
 	{ "123986", { "player.spell(123986).exists", "player.health < 85" }},
 	-- Zen Sphere on player
 	{ "124081", { "player.spell(124081).exists", "!player.buff(124081)" }},
+	--Healthstone
+	--{ "#5512", "player.health < 40" },
+	{ "!/run UseItemByName(5512)", "@intoBrew.Healthstone"},
 
 -- Stagger
-	{ "119582", (function() return intoBrew.DrinkStagger() end) }, 
+	--{ "119582", (function() return intoBrew.DrinkStagger() end) }, 
+	{ "119582", "@intoBrew.DrinkStagger" }, 
 
 -- Defensives
 	-- Elusive Brew 10 Stacks
@@ -94,23 +53,26 @@ ProbablyEngine.rotation.register_custom(268, "Into the Brew", {
 	{ "115203", "player.health <= 35" },
 	-- Guard
 	{ "123402", "player.health <= 35", "player"},
+	-- Self Dispell
+	{ "115450", "player.dispellable(115450)", "player"},
 
 -- Cooldowns
 	-- Xuen
-	{ "123904", "modifier.cooldowns"},
+	{ "123904", {"player.spell(123904).exists", "modifier.cooldowns"}},
 
 -- Rotation
 	--Touch of Death
-	{ "115080", (function() return intoBrew.touchdeath() end) },
+	--{ "115080", (function() return intoBrew.touchdeath() end) },
+	{ "115080", "@intoBrew.touchdeath" },
 	-- Keg Smash on CD
 	{ "121253" },
 	-- Blackout Kick 
-	{ "100784", { "!player.buff(115307)", "player.chi >= 2"} },
+	--{ "100784", { "!player.buff(115307)", "player.chi >= 2"} },
 	{{
 		{ "100784", "!player.buff(115307)"},
 		{ "100784", "player.buff(115307).duration < 3"},
 		{ "100784" }
-	}, "player.chi >= 3"},
+	}, "player.chi >= 2"},
 	-- Tiger Palm
 	{ "100787", "player.buff(125359).duration < 4" },
 	{ "100787", "player.energy <= 39"},
@@ -118,21 +80,29 @@ ProbablyEngine.rotation.register_custom(268, "Into the Brew", {
 	{ "115072", "player.health <= 85"},
 	-- Breath of Fire
 	{ "115181", {
-		"player.chi >= 4",
+		"player.chi >= 2",
+		--"player.buff(115307).duration >= 3",
 		"modifier.multitarget"
 	}}, 
 	--Spinning Crane Kick
 	{ "101546", {
 		"modifier.multitarget",
-		"!player.spell(116847).exists"		
+		"!player.spell(116847).exists",
+		"player.buff(115307).duration >= 3",	
 	}}, 
 	--Rushing Jade Wind
 	{ "116847", {
 		"modifier.multitarget" ,
 		"!player.buff(116847)",
-		"player.spell(116847).exists"		
+		"player.spell(116847).exists",
+		"player.buff(115307).duration >= 3",		
 	}}, 	
 	-- Jab 
 	{ "100780", "player.energy >= 40"},
+
+},	{
+	-- OOC Hotkeys
+	{ "115180", "modifier.lalt", "ground" }, -- Dizzying Haze
+ 	{ "115315", "modifier.rcontrol", "ground" }, -- Black Ox Statue
 
 })

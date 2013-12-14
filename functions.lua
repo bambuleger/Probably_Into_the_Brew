68520406829d725f59d98fa1a8c979e970dd8359
chumiiger <jonas.dreier@googlemail.com>
2013-12-14 05:11:23 +0100

    v1.5
    chat overlay / macros

--- functions.lua
+++ functions.lua
@@ -1,78 +1,203 @@
 --[[
     =======================================================
                          Into the Brew
-                             v1.4
+                             v1.5
     =======================================================
                 Brewmaster Monk Rotation for PE            
     https://probablyengine.com/forum/viewtopic.php?id=189
     =======================================================
 ]]--
 
--- Initialize tables
-if not intoBrew then intoBrew = {} end
+-----------------------------------------------------------------------------------------------------------------------------
+-- Initialize Tables -------------------------------------------------------------------------------------------------------- 
+-----------------------------------------------------------------------------------------------------------------------------
+local intoBrew = {} 
 
---[[local playerName = UnitName("player")
+intoBrew.queueSpell = nil
+intoBrew.queueTime = 0
+-----------------------------------------------------------------------------------------------------------------------------
+-- Notify Frame -- (c)xrn overlay for pqr -----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------
+    local function onUpdate(self,elapsed) 
+      if self.time < GetTime() - 2.5 then
+        if self:GetAlpha() == 0 then self:Hide() else self:SetAlpha(self:GetAlpha() - .05) end
+      end
+    end
+    itb = CreateFrame("Frame",nil,ChatFrame1) 
+    itb:SetSize(ChatFrame1:GetWidth(),30)
+    itb:Hide()
+    itb:SetScript("OnUpdate",onUpdate)
+    itb:SetPoint("TOPLEFT",0,150)
+    itb.text = itb:CreateFontString(nil,"OVERLAY","MovieSubtitleFont")
+    itb.text:SetAllPoints()
+    itb.texture = itb:CreateTexture()
+    itb.texture:SetAllPoints()
+    itb.texture:SetTexture(0,0,0,.50) 
+    itb.time = 0
+    function itb:message(message) 
+      self.text:SetText(message)
+      self:SetAlpha(1)
+      self.time = GetTime() 
+      self:Show() 
+    end
+    
+    local function onUpdate(self,elapsed) 
+      if self.time < GetTime() - 2.8 then
+        if self:GetAlpha() == 0 then self:Hide() else self:SetAlpha(self:GetAlpha() - .05) end
+      end
+    end
+    itb = CreateFrame("Frame",nil,ChatFrame1) 
+    itb:SetSize(ChatFrame1:GetWidth(),30)
+    itb:Hide()
+    itb:SetScript("OnUpdate",onUpdate)
+    itb:SetPoint("TOP",0,0)
+    itb.text = itb:CreateFontString(nil,"OVERLAY","MovieSubtitleFont")
+    itb.text:SetAllPoints()
+    itb.texture = itb:CreateTexture()
+    itb.texture:SetAllPoints()
+    itb.texture:SetTexture(0,0,0,.50) 
+    itb.time = 0
+    function itb:message(message) 
+      self.text:SetText(message)
+      self:SetAlpha(1)
+      self.time = GetTime() 
+      self:Show() 
+    end
+-----------------------------------------------------------------------------------------------------------------------------
+-- Macros ------------------------------------------------------------------------------------------------------------------- 
+-----------------------------------------------------------------------------------------------------------------------------
+ProbablyEngine.command.register('into', function(msg, box)
+  local command, text = msg:match("^(%S*)%s*(.-)$")
+-- Toggle -------------------------------------------------------------------------------------------------------------------
+  if command == 'toggle' then
+    if ProbablyEngine.toggle.states.MasterToggle then
+        ProbablyEngine.buttons.toggle('MasterToggle')
+        itb:message("|cFFB30000Into the Brew off")
+    else
+        ProbablyEngine.buttons.toggle('MasterToggle')
+        itb:message("|cFF00B34AInto the Brew on")
+    end
+  end
+  if command == 'kick' then
+    if ProbablyEngine.toggle.states.interrupt then
+      ProbablyEngine.buttons.toggle('interrupt')
+      itb:message("|cFFB30000Interrupts off")
+    else
+      ProbablyEngine.buttons.toggle('interrupt')
+      itb:message("|cFF00B34AInterrupts on")
+    end
+  end
 
-StaticPopupDialogs["welcome"] = {
-        maxLetters = 1200,
-        editBoxWidth = 2150,
-        OnShow = function(self)
-    end,
-    text = " Welcome back " .. playerName .. "\nInto the Brew v1.3 loaded\nPlease comment / report bugs at\nhttp://tinyurl.com/intobrew",
-    button1 = "Have fun!",
-    OnAccept = function()
-    end,
-    timeout = 20,
-    whileDead = true,
-    hideOnEscape = true,
-    preferredIndex = 3,  
-}
-StaticPopup_Show ("welcome")]]--
+  if command == 'xuen' then
+    if ProbablyEngine.toggle.states.cooldowns then
+      ProbablyEngine.buttons.toggle('cooldowns')
+      itb:message("|cFFB30000Xuen off")
+    else
+      ProbablyEngine.buttons.toggle('cooldowns')
+      itb:message("|cFF00B34AXuen on")
+    end
+  end
 
--- Thank you Mavmins for the code
---[[ProbablyEngine.listener.register("intoBrew", "MODIFIER_STATE_CHANGED", function(...)
-        local key                        = select(1, ...)
-        local isDown                = select(2, ...)
-        USE_AOE = ProbablyEngine.config.data.button_states.multitarget
-        USE_INTRPT = ProbablyEngine.config.data.button_states.interrupt
-               
-       if  (key == "RSHIFT" and isDown == 1) then --cooldowns toggle
-                if USE_CDS then 
-                        USE_CDS = false 
-                        print("Cooldowns: |cFFFF0000Disabled")        
-                        ProbablyEngine.buttons.toggle('cooldowns')
-                else 
-                        USE_CDS = true 
-                        print("Cooldowns: |cFF00FF00Enabled")
-                        ProbablyEngine.buttons.toggle('cooldowns')
-                end
-        end    
+  if command == 'aoe' then
+    if ProbablyEngine.toggle.states.multitarget then
+      ProbablyEngine.buttons.toggle('multitarget')
+      itb:message("|cFFB30000AoE off")
+    else
+      ProbablyEngine.buttons.toggle('multitarget')
+      itb:message("|cFF00B34AAoE on")
+    end
+  end
+  
+  if command == 'taunt' then
+    if ProbablyEngine.toggle.states.taunt then
+      ProbablyEngine.buttons.toggle('taunt')
+      itb:message("|cFFB30000SoO Auto Taunt off")
+    else
+      ProbablyEngine.buttons.toggle('taunt')
+      itb:message("|cFF00B34ASoO Auto Taunt on")
+    end
+  end
 
-        if  (key == "LCTRL" and isDown == 1) then  --aoe toggle
-                if USE_AOE then
-                        USE_AOE = false
-                        print("AoE: |cFFFF0000Disabled")
-                        ProbablyEngine.buttons.toggle('multitarget')                        
-                else
-                        USE_AOE = true
-                        print("AoE: |cFF00FF00Enabled")        
-                        ProbablyEngine.buttons.toggle('multitarget')
-                end
-        end 
+  if command == 'def' then
+    if ProbablyEngine.toggle.states.guard then
+      ProbablyEngine.buttons.toggle('def')
+      itb:message("|cFFB30000Defensive Cooldowns off")
+    else
+      ProbablyEngine.buttons.toggle('def')
+      itb:message("|cFF00B34ADefensive Cooldowns on")
+    end
+  end
 
-        if  (key == "RSHIFT" and isDown == 1) then  --interrupt toggle
-                if USE_INTRPT then
-                        USE_INTRPT = false
-                        print("Interrupts: |cFFFF0000Disabled")
-                        ProbablyEngine.buttons.toggle('interrupt')                        
-                else
-                        USE_INTRPT = true
-                        print("Interrupts: |cFF00FF00Enabled")        
-                        ProbablyEngine.buttons.toggle('interrupt')
-                end
-        end       
-end)]]--
- 
+  if command == 'ksmash' then
+    if ProbablyEngine.toggle.states.kegsmash then
+      ProbablyEngine.buttons.toggle('kegsmash')
+      itb:message("|cFFB30000Keg Smash off")
+    else
+      ProbablyEngine.buttons.toggle('kegsmash')
+      itb:message("|cFF00B34AKeg Smash on")
+    end
+  end
+-- Spell Queue -- thank you merq for basic code -----------------------------------------------------------------------------
+  if command == "qGuard" or command == 123402 then
+    intoBrew.queueSpell = 123402
+    itb:message("Guard queued")
+  elseif command == "qBrew" or command == 115203 then
+    intoBrew.queueSpell = 115203
+    itb:message("Fortifying Brew queued")
+  elseif command == "qZen" or command == 115176 then
+    intoBrew.queueSpell = 115176
+    itb:message("Zen Meditation queued")
+  elseif command == "qAvert" or command == 115213 then
+    intoBrew.queueSpell = 115213
+    itb:message("Avert Harm queued")
+  elseif command == "qTfour" then
+    if select(2,GetTalentRowSelectionInfo(4)) == 10 then
+        intoBrew.queueSpell = 116844
+        itb:message("Ring of Peace queued") 
+    elseif select(2,GetTalentRowSelectionInfo(4)) == 11 then
+        intoBrew.queueSpell = 119392
+        itb:message("Charging Ox Wave queued") 
+    elseif select(2,GetTalentRowSelectionInfo(4)) == 12 then
+        intoBrew.queueSpell = 119381
+        itb:message("Leg Sweep queued") 
+    end
+  elseif command == "qTfive" then
+    if select(2,GetTalentRowSelectionInfo(5)) == 14 then
+        intoBrew.queueSpell = 122278
+        itb:message("Dampen Harm queued") 
+    elseif select(2,GetTalentRowSelectionInfo(5)) == 15 then
+        intoBrew.queueSpell = 122783
+        itb:message("Diffuse Magic queued") 
+    end
+  else
+    intoBrew.queueSpell = nil
+  end
+  if intoBrew.queueSpell ~= nil then intoBrew.queueTime = GetTime() end
+end)
+-----------------------------------------------------------------------------------------------------------------------------
+-- Spell Queue Check -- thank you merq for basic code ----------------------------------------------------------------------- 
+-----------------------------------------------------------------------------------------------------------------------------
+intoBrew.checkQueue = function (spellId)
+    if (GetTime() - intoBrew.queueTime) > 4 then
+        intoBrew.queueTime = 0
+        intoBrew.queueSpell = nil
+    return false
+    else
+    if intoBrew.queueSpell then
+        if intoBrew.queueSpell == spellId then
+            if ProbablyEngine.parser.lastCast == GetSpellName(spellId) then
+                intoBrew.queueSpell = nil
+                intoBrew.queueTime = 0
+            end
+        return true
+        end
+    end
+    end
+    return false
+end
+-----------------------------------------------------------------------------------------------------------------------------
+-- Touch of Death ----------------------------------------------------------------------------------------------------------- 
+-----------------------------------------------------------------------------------------------------------------------------
 function intoBrew.touchdeath ()
     local GlyphSockets = NUM_GLYPH_SLOTS
     if IsSpellKnown(115080)
@@ -89,7 +214,9 @@ function intoBrew.touchdeath ()
         end
      end    
 end
-
+-----------------------------------------------------------------------------------------------------------------------------
+-- Stagger ------------------------------------------------------------------------------------------------------------------ 
+-----------------------------------------------------------------------------------------------------------------------------
 function intoBrew.StaggerValue ()    
     local staggerLight, _, iconLight, _, _, remainingLight, _, _, _, _, _, _, _, _, valueStaggerLight, _, _ = UnitAura("player", GetSpellInfo(124275), "", "HARMFUL")
     local staggerModerate, _, iconModerate, _, _, remainingModerate, _, _, _, _, _, _, _, _, valueStaggerModerate, _, _ = UnitAura("player", GetSpellInfo(124274), "", "HARMFUL")
@@ -116,7 +243,9 @@ function intoBrew.DrinkStagger()
     end
     return false
 end
-
+-----------------------------------------------------------------------------------------------------------------------------
+-- Healthstone -------------------------------------------------------------------------------------------------------------- 
+-----------------------------------------------------------------------------------------------------------------------------
 local PlayerHP = 100 * UnitHealth("player") / UnitHealthMax("player")
 
 function intoBrew.Healthstone(...)
@@ -125,7 +254,8 @@ function intoBrew.Healthstone(...)
     and ( select(2, GetItemCooldown(5512)) == 0 ) then
         return true
     end
-end
- 
--- Register library
+end 
+-----------------------------------------------------------------------------------------------------------------------------
+-- Register Library --------------------------------------------------------------------------------------------------------- 
+-----------------------------------------------------------------------------------------------------------------------------
 ProbablyEngine.library.register("intoBrew", intoBrew)
--- rotation.lua
+++ rotation.lua
@@ -1,7 +1,7 @@
 --[[
 	=======================================================
 	                     Into the Brew
-	                         v1.4
+	                         v1.5
 	=======================================================
 	            Brewmaster Monk Rotation for PE            
 	https://probablyengine.com/forum/viewtopic.php?id=189
@@ -11,13 +11,15 @@
 
 ProbablyEngine.rotation.register_custom(268, "Into the Brew", {
 
--- Hotkeys
+-----------------------------------------------------------------------------------------------------------------------------
+-- Hotkeys ------------------------------------------------------------------------------------------------------------------ 
+-----------------------------------------------------------------------------------------------------------------------------
 	{ "pause", "modifier.lshift"},
  	{ "115180", "modifier.lalt", "ground" }, -- Dizzying Haze
  	{ "115315", "modifier.rcontrol", "ground" }, -- Black Ox Statue
-
-
--- Buff
+-----------------------------------------------------------------------------------------------------------------------------
+-- Buffs -------------------------------------------------------------------------------------------------------------------- 
+-----------------------------------------------------------------------------------------------------------------------------
 	{ "115921", -- Legacy of the Emperor
     	{
       	"!player.buff(117666).any", -- Legacy of the Emperor Buff
@@ -26,32 +28,51 @@ ProbablyEngine.rotation.register_custom(268, "Into the Brew", {
       	"!player.buff(20217).any" -- Blessing of Kings
     	}
  	},
-
--- Interrupts
+-----------------------------------------------------------------------------------------------------------------------------
+-- Queued Spells ------------------------------------------------------------------------------------------------------------ 
+-----------------------------------------------------------------------------------------------------------------------------
+	{ "!123402", "@intoBrew.checkQueue(123402)" },
+	{ "!115203", "@intoBrew.checkQueue(115203)" },
+	{ "!115176", "@intoBrew.checkQueue(115176)" },
+	{ "!115213", "@intoBrew.checkQueue(115213)" },
+	{ "!116844", "@intoBrew.checkQueue(116844)" },
+	{ "!119392", "@intoBrew.checkQueue(119392)" },
+	{ "!119381", "@intoBrew.checkQueue(119381)" },
+	{ "!122280", "@intoBrew.checkQueue(122280)" },
+	{ "!122278", "@intoBrew.checkQueue(122278)" },
+	{ "!122783", "@intoBrew.checkQueue(122783)" },
+-----------------------------------------------------------------------------------------------------------------------------
+-- Interrupts --------------------------------------------------------------------------------------------------------------- 
+-----------------------------------------------------------------------------------------------------------------------------
 	{ "117368", "modifier.interrupts" }, --Grapple Weapon (Disarm)
 	{ "116705", "modifier.interrupts" }, --Spear Hand Strike
-
--- Selfheal Talents T2 
+-----------------------------------------------------------------------------------------------------------------------------
+-- Selfheal Talents T2 ------------------------------------------------------------------------------------------------------ 
+-----------------------------------------------------------------------------------------------------------------------------
 	{ "115098", { "player.spell(115098).exists", "player.health < 85" }}, -- Chi Wave
 	{ "123986", { "player.spell(123986).exists", "player.health < 85" }}, -- Chi Burst
 	{ "124081", { "player.spell(124081).exists", "!player.buff(124081)" }}, -- Zen Sphere on player
 	{ "!/run UseItemByName(5512)", "@intoBrew.Healthstone"}, --Healthstone
-
--- Stagger
+-----------------------------------------------------------------------------------------------------------------------------
+-- Stagger ------------------------------------------------------------------------------------------------------------------ 
+-----------------------------------------------------------------------------------------------------------------------------
 	{ "119582", "@intoBrew.DrinkStagger" }, 
-
--- Defensives
+-----------------------------------------------------------------------------------------------------------------------------
+-- Defensives---------------------------------------------------------------------------------------------------------------- 
+-----------------------------------------------------------------------------------------------------------------------------
 	{ "115308", "player.buff(128939).count >= 10" }, -- Elusive Brew 10 Stacks
-	{ "115203", "player.health <= 35" }, -- Fortifying Brew
-	{ "123402", {"player.health <= 75", "toggle.guard"}, "player"}, -- Guard
+	{ "115203", {"player.health <= 35", "toggle.def" }, "player" }, -- Fortifying Brew
+	{ "123402", {"player.health <= 50", "toggle.def" }, "player" }, -- Guard
 	{ "115450", "player.dispellable(115450)", "player"}, -- Self Dispell
-
--- Cooldowns
+-----------------------------------------------------------------------------------------------------------------------------
+-- Cooldowns ---------------------------------------------------------------------------------------------------------------- 
+-----------------------------------------------------------------------------------------------------------------------------
 	{ "123904", {"player.spell(123904).exists", "modifier.cooldowns"}}, -- Xuen
-
--- Rotation --------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------
+-- Main Rotation ------------------------------------------------------------------------------------------------------------ 
+-----------------------------------------------------------------------------------------------------------------------------
 	{ "115080", "@intoBrew.touchdeath" }, --Touch of Death
-	{ "121253", "!toggle.kegsmash" }, -- Keg Smash on CD
+	{ "121253", "toggle.kegsmash" }, -- Keg Smash on CD
 	-- Blackout Kick 
 	{{
 		{ "100784", "!player.buff(115307)"},
@@ -80,20 +101,43 @@ ProbablyEngine.rotation.register_custom(268, "Into the Brew", {
 		"player.buff(115307).duration >= 3",		
 	}}, 	
 	{ "100780", "player.energy >= 40"},-- Jab
---------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------
+-- SoO Taunt Events --------------------------------------------------------------------------------------------------------- 
+-----------------------------------------------------------------------------------------------------------------------------
+	{ "115546", {"target.id(71543)", "!player.debuff(143436)", "focus.debuff(143436).count >= 1", "toggle.taunt"}}, 	-- Immerseus 1 Stack
+	{ "115546", {"target.id(72276)", "!player.debuff(146124)", "focus.debuff(146124).count >= 4", "toggle.taunt"}}, 	-- Norushen 4 Stacks
+	{ "115546", {"target.id(71734)", "!player.debuff(144358)", "focus.debuff(144358).count >= 1", "toggle.taunt"}}, 	-- Sha of Pride 1 Stack
+	{ "115546", {"target.id(71466)", "!player.debuff(144467)", "focus.debuff(144467).count >= 3", "toggle.taunt"}}, 	-- Iron Juggernaut 3 Stacks
+	{ "115546", {"target.id(71859)", "!player.debuff(144215)", "focus.debuff(144215).count >= 5", "toggle.taunt"}}, 	-- Dark Shamans 5 Stacks
+	{ "115546", {"target.id(71515)", "!player.debuff(143494)", "focus.debuff(143494).count >= 3", "toggle.taunt"}}, 	-- Nazgrim 3 Stacks
+	{ "115546", {"target.id(71454)", "!player.debuff(142990)", "focus.debuff(142990).count >= 13", "toggle.taunt"}}, 	-- Malkorok 13 Stacks
+	{ "115546", {"target.id(71504)", "!player.debuff(143385)", "focus.debuff(143385).count >= 3", "toggle.taunt"}},		-- Blackfuse 3 Stacks !!!
+	{ "115546", {"target.id(71529)", "!player.debuff(143426)", "focus.debuff(143426).count >= 3", "toggle.taunt"}}, 	-- Thok 3 Stacks Fearsome Roar
+	{ "115546", {"target.id(71529)", "!player.debuff(143780)", "focus.debuff(143780).count >= 3", "toggle.taunt"}}, 	-- Thok 3 Stacks Acid Breath
+	{ "115546", {"target.id(71529)", "!player.debuff(143773)", "focus.debuff(143773).count >= 3", "toggle.taunt"}}, 	-- Thok 3 Stacks Freezing Breath
+	{ "115546", {"target.id(71865)", "!player.debuff(145183)", "focus.debuff(145183).count >= 3", "toggle.taunt"}}, 	-- Garrosh 3 Stacks Gripping Despair
+	{ "115546", {"target.id(71865)", "!player.debuff(145195)", "focus.debuff(145195).count >= 3", "toggle.taunt"}}, 	-- Garrosh 3 Stacks Empowered Gripping Despair
+-----------------------------------------------------------------------------------------------------------------------------
+-- OOC Hotkeys -------------------------------------------------------------------------------------------------------------- 
+-----------------------------------------------------------------------------------------------------------------------------
 },	{
-	-- OOC Hotkeys
 	{ "115180", "modifier.lalt", "ground" }, -- Dizzying Haze
  	{ "115315", "modifier.rcontrol", "ground" }, -- Black Ox Statue
-
-},
---------------------------------------------------------------------------------------------------
+	},
+-----------------------------------------------------------------------------------------------------------------------------
+-- Custom Toggle ------------------------------------------------------------------------------------------------------------ 
+-----------------------------------------------------------------------------------------------------------------------------
 function()
 ProbablyEngine.toggle.create(
-    'guard',
-    'Interface\\Icons\\ability_monk_guard.png‎',
-    'Auto Guard Toggle',
-	'Enable or Disable Guard at 75% Health')
+    'def',
+    'Interface\\Icons\\INV_SummerFest_Symbol_Medium.png‎',
+    'Defensive CDs Toggle',
+	'Enable or Disable usage of Guard / Fortifying Brew')
+ProbablyEngine.toggle.create(
+    'taunt',
+    'Interface\\Icons\\Spell_Magic_PolymorphRabbit.png‎',
+    'SoO Auto Taunt Toggle',
+	'Enable or Disable Auto Taunt in SoO\nImmerseus 1 Stack\nNorushen 4 Stacks\nSha of Pride 1 Stack\nIron Juggernaut 3 Stacks\nDark Shamans 5 Stacks\nGeneral Nazgrim 3 Stacks\nMalkorok 13 Stacks\nBlackfuse 3 Stacks\nThok 3 Stacks\nGarrosh 3 Stacks\nSET 2ND TANK TO FOCUS')
 ProbablyEngine.toggle.create(
     'kegsmash',
     'Interface\\Icons\\achievement_brewery_2.png‎',
